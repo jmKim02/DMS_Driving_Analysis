6b78b224adfd367d518e5eecfe554934f18edb43
Kim Jongmin <jmkim990226@gmail.com>
2025-03-13 13:29:18 +0900

    "add Initial settings"

---
+++ drivinganalysis/src/main/java/com/sejong/drivinganalysis/DrivinganalysisApplication.java
@@ -0,0 +1,13 @@
+package com.sejong.drivinganalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivinganalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivinganalysisApplication.class, args);
+	}
+
+}
